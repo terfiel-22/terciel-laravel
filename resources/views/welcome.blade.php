e88e80f11d5634a336c0a85214ed2ba01ee2cfa7
terfiel-22 <dietherfiel@gmail.com>
2023-07-16 16:40:27 +0800

    Small update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,14 @@
 @section('content')
 <main class="content">
     <div class="container-fluid p-0">
-        <div class="row mb-2 mb-xl-3">
-            <div class="col-auto d-none d-sm-block">
-                <h3><strong>Landing</strong> Page</h3>
+
+        <div class="mb-3">
+            <h1 class="h3 d-inline align-middle">Landing Page</h1>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <p>Display Categories</p>
             </div>
         </div>
     </div>
