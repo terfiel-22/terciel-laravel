2f08d96aa45166c3475df835ee7823c8b9482495
terfiel-22 <dietherfiel@gmail.com>
2023-07-24 18:16:14 +0800

    Update home page

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,32 +2,53 @@
 
 @section('content')
 <section class="banner">
-    <div
-        class="swiffy-slider slider-item-ratio slider-item-ratio-2x1 slider-item-nogap slider-item-nosnap-touch slider-nav-square slider-nav-dark slider-nav-sm slider-nav-page slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein slider-nav-animation-slow mb-5">
-        <ul class="slider-container">
-            <li id="slide1" class="bg-image">
-                <img src="https://swiffyslider.com/assets/img/photos/img1.webp" class="w-100" />
-                <div class="mask text-light d-flex justify-content-center flex-column text-start image-text-container">
-                    <div class="col-8 ms-4">
-                        <h1 class="image-text">Create a lifestyle you desire.</h1>
-                    </div>
+    <div class="swiffy-slider slider-item-nogap slider-item-ratio slider-item-ratio-2x1 slider-item-nosnap-touch slider-nav-square slider-nav-dark slider-nav-sm slider-nav-page slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein slider-nav-animation-slow"
+        id="animatedText">
+
+        <ul class="slider-container" id="container1">
+            <li id="slide1" class="slide-visible">
+                <div>
+                    <img src="https://imgv3.fotor.com/images/share/wonderland-girl-generated-by-Fotor-ai-art-generator.jpg"
+                        alt="..." loading="lazy" class="w-100">
+                    <h1
+                        class="display-1 text-dark custom-animated-text position-absolute top-50 start-50 translate-middle">
+                        Woman</h1>
                 </div>
             </li>
-            <li id="slide1" class="bg-image">
-                <img src="https://swiffyslider.com/assets/img/photos/img2.webp" class="w-100" />
-                <div class="mask text-light d-flex justify-content-center flex-column text-start image-text-container">
-                    <div class="col-9 ms-4">
-                        <h1 class="image-text">Engaging purposeful and creative.</h1>
-                    </div>
+            <li id="slide2">
+                <div>
+                    <img src="{{ asset('images/avatars/avatar-3.jpg') }}" alt="..." loading="lazy" class="w-100">
+                    <h1
+                        class="display-1 text-dark custom-animated-text position-absolute top-50 start-50 translate-middle">
+                        Woman</h1>
                 </div>
             </li>
-            <li id="slide3"><img src="https://swiffyslider.com/assets/img/photos/img3.webp" alt="..." loading="lazy">
+            <li id="slide3">
+                <div>
+                    <img src="https://w0.peakpx.com/wallpaper/693/564/HD-wallpaper-empty-snowy-road-at-night.jpg"
+                        alt="..." loading="lazy" class="w-100">
+                    <h1
+                        class="display-1 text-dark custom-animated-text position-absolute top-50 start-50 translate-middle">
+                        Aston</h1>
+                </div>
             </li>
-            <li id="slide4"><img src="https://swiffyslider.com/assets/img/photos/img4.webp" alt="..." loading="lazy">
+            <li id="slide4">
+                <div>
+                    <img src="https://swiffyslider.com/assets/img/photos/img4.webp" alt="..." loading="lazy"
+                        class="w-100">
+                    <h1
+                        class="display-1 text-dark custom-animated-text position-absolute top-50 start-50 translate-middle">
+                        Mountain</h1>
+                </div>
             </li>
-            <li id="slide5"><img src="https://swiffyslider.com/assets/img/photos/img5.webp" alt="..." loading="lazy">
-            </li>
-            <li id="slide6"><img src="https://swiffyslider.com/assets/img/photos/img6.webp" alt="..." loading="lazy">
+            <li id="slide5">
+                <div>
+                    <img src="https://swiffyslider.com/assets/img/photos/img5.webp" alt="..." loading="lazy"
+                        class="w-100">
+                    <h1
+                        class="display-1 text-dark custom-animated-text position-absolute top-50 start-50 translate-middle">
+                        Car</h1>
+                </div>
             </li>
         </ul>
 
@@ -40,19 +61,21 @@
             <button aria-label="Go to slide"></button>
             <button aria-label="Go to slide"></button>
             <button aria-label="Go to slide"></button>
-            <button aria-label="Go to slide"></button>
         </div>
     </div>
-    <div
-        class="swiffy-slider slider-item-show5 slider-item-nogap slider-item-ratio slider-item-ratio-1x1 slider-item-snapstart slider-item-nosnap-touch slider-nav-square slider-nav-dark slider-nav-sm slider-nav-visible slider-nav-page slider-nav-animation slider-nav-animation-fadein slider-nav-animation-slow">
-        <ul class="slider-container">
+
+    {{-- <div
+        class="swiffy-slider slider-item-show2 slider-item-reveal slider-nav-outside slider-nav-chevron slider-nav-visible slider-indicators-outside slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein">
+        <ul class="slider-container py-4">
             @foreach ($allPosts as $post_item)
-            <li>
-                <div class="px-1" id="slide1">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ asset($post_item->image) }}" alt="Unsplash">
-                        <div class="card-header">
-                            <h5 class="card-title mb-0">{{ $post_item->name }}</h5>
+            <li class="">
+                <div class="card shadow h-100">
+                    <div class="ratio ratio-16x9">
+                        <img src="{{ asset($post_item->image) }}" class="card-img-top" loading="lazy" alt="...">
+                    </div>
+                    <div class="card-body p-3 p-xl-5">
+                        <h3 class="card-title h5">{{ $post_item->name }}</h3>
+                        <div><a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
@@ -60,10 +83,11 @@
             @endforeach
         </ul>
 
-        <button type="button" class="slider-nav"></button>
-        <button type="button" class="slider-nav slider-nav-next"></button>
-    </div>
+        <button type="button" class="slider-nav" aria-label="Go left"></button>
+        <button type="button" class="slider-nav slider-nav-next" aria-label="Go left"></button>
+    </div> --}}
 </section>
+
 <main class="content">
     <div class="container-fluid p-0">
         <div class="row">
