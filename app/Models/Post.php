289bb4eaec718b4b9b070007d73ac377a8a828e8
terfiel-22 <dietherfiel@gmail.com>
2023-07-24 19:40:26 +0800

    Update home page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -40,4 +40,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function author() : BelongsTo
+    {
+        return $this->belongsTo(User::class,'created_by','id');
+    }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="banner">
-    <div class="swiffy-slider slider-item-nogap slider-item-ratio slider-item-ratio-2x1 slider-item-nosnap-touch slider-nav-square slider-nav-dark slider-nav-sm slider-nav-page slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein slider-nav-animation-slow"
+    <div class="swiffy-slider slider-item-nogap slider-item-ratio slider-item-nosnap-touch slider-nav-square slider-nav-dark slider-nav-sm slider-nav-page slider-nav-autoplay slider-nav-autopause slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein slider-nav-animation-slow"
         id="animatedText">
 
         <ul class="slider-container" id="container1">
@@ -64,27 +64,108 @@
         </div>
     </div>
 
-    {{-- <div
-        class="swiffy-slider slider-item-show2 slider-item-reveal slider-nav-outside slider-nav-chevron slider-nav-visible slider-indicators-outside slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein">
-        <ul class="slider-container py-4">
-            @foreach ($allPosts as $post_item)
-            <li class="">
-                <div class="card shadow h-100">
-                    <div class="ratio ratio-16x9">
-                        <img src="{{ asset($post_item->image) }}" class="card-img-top" loading="lazy" alt="...">
+    <div class="m-5">
+        <div
+            class="swiffy-slider slider-item-show2 slider-item-reveal slider-nav-outside slider-nav-chevron slider-nav-visible slider-indicators-outside slider-indicators-round slider-indicators-dark slider-nav-animation slider-nav-animation-fadein">
+            <ul class="slider-container p-4 bg-light">
+                @foreach ($allPosts as $post_item)
+                <li>
+                    <div class="card shadow h-100">
+                        <div class="ratio ratio-16x9">
+                            <img src="{{ asset($post_item->image) }}" class="card-img-top" loading="lazy" alt="...">
+                        </div>
+                        <div class="card-body p-3 d-flex flex-column justify-content-between">
+                            <div>
+                                <h2>{{ $post_item->name }}</h2>
+                                <p><span data-feather="user" class="me-2"></span>{{ $post_item->author->name }}</p>
+                                <p><span data-feather="clock" class="me-2"></span>{{ $post_item->created_at }}</p>
+                            </div>
+                            <a href="{{ url('blog/'.$post_item->category->slug.'/'.$post_item->slug) }}"
+                                class="btn btn-danger p-3">READ</a>
+                        </div>
                     </div>
-                    <div class="card-body p-3 p-xl-5">
-                        <h3 class="card-title h5">{{ $post_item->name }}</h3>
-                        <div><a href="#" class="btn btn-primary">Go somewhere</a>
+                </li>
+                @endforeach
+            </ul>
+
+            <button type="button" class="slider-nav" aria-label="Go left"></button>
+            <button type="button" class="slider-nav slider-nav-next" aria-label="Go left"></button>
+        </div>
+    </div>
+
+    {{-- <div
+        class="swiffy-slider slider-nav-round slider-nav-animation slider-nav-animation-fadein slider-item-first-visible">
+        <ul class="slider-container">
+            <li class="slide-visible">
+                <div class="card rounded-0 h-100">
+                    <div class="row g-0 h-100">
+                        <div class="col-md-6 col-xl-5 d-flex align-items-center p-2 p-md-3 p-xl-5">
+                            <div class="card-body p-1 p-md-3 p-xl-5">
+                                <p class="lead">Why use this slider</p>
+                                <h2 class="card-title" style="color:#DF1E1E;">Swiffy Slider Benefits</h2>
+                                <p class="card-text mt-3">Super fast lightweight slider and carousel with amazing touch
+                                    support and user experience.</p>
+                                <p>Super simple setup using just markup and few powerful configuration options</p>
+                                <p class="card-text"><small class="text-muted">Remember to check out on mobile</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-7">
+                            <img src="../assets/img/photos/img1.webp" class="card-img d-none d-md-block" loading="lazy"
+                                style="height: 100%; object-fit: cover;" alt="...">
+                            <img src="../assets/img/photos/img1.webp" class="card-img d-block d-md-none" loading="lazy"
+                                style="width: 100%; object-fit: cover;" alt="...">
                         </div>
                     </div>
                 </div>
             </li>
-            @endforeach
+            <li class="">
+                <div class="card rounded-0 h-100">
+                    <div class="row g-0 h-100">
+                        <div class="col-md-6 col-xl-7">
+                            <img src="../assets/img/photos/img2.webp" class="card-img d-none d-md-block" loading="lazy"
+                                style="height: 100%; object-fit: cover" alt="...">
+                            <img src="../assets/img/photos/img2.webp" class="card-img d-block d-md-none" loading="lazy"
+                                style="width: 100%; object-fit: cover" alt="...">
+                        </div>
+                        <div class="col-md-6 col-xl-5 d-flex align-items-center p-2 p-md-3 p-xl-5">
+                            <div class="card-body p-1 p-md-3 p-xl-5">
+                                <p class="lead">Dreaming about cars</p>
+                                <h2 class="card-title" style="color:#DF1E1E;">Vintage cars from another era</h2>
+                                <p class="card-text mt-3">First, there’s the design of the car itself. Classic cars were
+                                    created very much in an analogue world where designers used pencil and paper to
+                                    create elegant shapes and flowing lines that would just not be possible
+                                    on the computer-based design software used by modern car designers.</p>
+                                <p class="card-text"><small class="text-muted">Handcrafted like good code</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="card rounded-0 h-100 text-white">
+                    <img src="https://c4.wallpaperflare.com/wallpaper/862/49/164/ultrawide-artwork-event-horizon-black-holes-midjourney-ai-hd-wallpaper-preview.jpg"
+                        class="card-img h-100" style="max-height: 100%; max-width: 100%; object-fit: cover" alt="...">
+                    <div
+                        class="card-img-overlay d-flex align-items-start align-items-md-center justify-content-center flex-column">
+                        <h2 class="card-title h1 d-block d-md-inline-block">The Aston Martin DB5</h2>
+                        <p class="card-text lead col-md-8 col-lg-6 text-dark">The Aston Martin DB5 is a British luxury
+                            grand
+                            tourer (GT) that was made by Aston Martin and designed by the Italian coachbuilder
+                            Carrozzeria Touring Superleggera. </p>
+                    </div>
+                </div>
+            </li>
         </ul>
 
         <button type="button" class="slider-nav" aria-label="Go left"></button>
         <button type="button" class="slider-nav slider-nav-next" aria-label="Go left"></button>
+
+        <div class="slider-indicators">
+            <button class="active" aria-label="Go to slide"></button>
+            <button aria-label="Go to slide" class=""></button>
+            <button aria-label="Go to slide"></button>
+        </div>
     </div> --}}
 </section>
 
