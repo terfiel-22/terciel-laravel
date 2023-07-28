348658b46695a4cec7f207e8bdcf82225ffd2044
terfiel-22 <dietherfiel@gmail.com>
2023-07-28 11:42:29 +0800

    Add banner slides

---
+++ app/Http/Controllers/Admin/BannerSlideController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class BannerSlideController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
